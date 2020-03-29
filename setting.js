ac6fe20875941135cf8fca7b46bcefd726434113
tokjin <41167277+tokjin@users.noreply.github.com>
2020-03-29 16:07:50 +0900

    v1.0.2
    htmlアンエスケープを追加

--- setting.js
+++ setting.js
@@ -8,7 +8,7 @@ let getParam = (name, url) => {
     return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
 
-const currentVer = 'v1.0.1';
+const currentVer = 'v1.0.2';
 
 // OPENRECのチャンネルID(https://www.openrec.tv/user/*****)
 let channelId = getParam('channelId') || '';
